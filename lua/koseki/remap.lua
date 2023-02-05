279839342e7b58b7b035daff1bfb6f27ee2021cf
KosekiDev <vincent.brehaut@gmail.com>
2023-02-05 22:05:05 +0100

    Ajout de deux raccourcis leader dd et leader da pour afficher l'explorateur de fichiers

--- lua/koseki/remap.lua
+++ lua/koseki/remap.lua
@@ -1,7 +1,9 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
+vim.keymap.set("n", "<leader>dd", ":25Lexplore %:p:h<CR>")
+vim.keymap.set("n", "<leader>da", ":25Lexplore<CR>")
+
 vim.keymap.set("n", "<leader>u", ":UndotreeShow<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
