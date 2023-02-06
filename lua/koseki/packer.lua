27a71aea9e337146c3b9ec9e5f3ad44ac7c60a61
KosekiDev <vincent.brehaut@gmail.com>
2023-02-06 15:25:28 +0100

    Suppression d'un plugin inutile

--- lua/koseki/packer.lua
+++ lua/koseki/packer.lua
@@ -35,7 +35,6 @@ return require('packer').startup(function(use)
     use 'tpope/vim-surround'
 
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-    use('nvim-treesitter/playground')
     use('theprimeagen/harpoon')
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
