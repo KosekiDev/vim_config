2fa5b247947d328a30973c4d1f6ef27e6abfbfcf
KosekiDev <vincent.brehaut@gmail.com>
2023-02-14 19:41:00 +0100

    Suppression d'un parser en doublon pour le javascript/typescript

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -22,6 +22,3 @@ require 'nvim-treesitter.configs'.setup {
         additional_vim_regex_highlighting = false,
     },
 }
-
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
