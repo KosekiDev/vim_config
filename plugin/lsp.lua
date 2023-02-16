be51b1f3a653213cb424da042ccddb905397e367
KosekiDev <vincent.brehaut@gmail.com>
2023-02-16 21:21:28 +0100

    Correction d'un conflit entre mason lsp et lspzero

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -48,26 +48,26 @@ vim.diagnostic.config({
     virtual_text = true,
 })
 
-local null_ls = require('null-ls')
-local null_opts = lsp.build_options('null-ls', {})
-
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-null_ls.setup({
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = bufnr })
-                end,
-            })
-        end
-    end,
-    sources = {
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.rustfmt,
-    }
-})
+-- local null_ls = require('null-ls')
+-- local null_opts = lsp.build_options('null-ls', {})
+--
+-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+--
+-- null_ls.setup({
+--     on_attach = function(client, bufnr)
+--         if client.supports_method("textDocument/formatting") then
+--             vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+--             vim.api.nvim_create_autocmd("BufWritePre", {
+--                 group = augroup,
+--                 buffer = bufnr,
+--                 callback = function()
+--                     vim.lsp.buf.format({ bufnr = bufnr })
+--                 end,
+--             })
+--         end
+--     end,
+--     sources = {
+--         null_ls.builtins.formatting.prettier,
+--         null_ls.builtins.formatting.rustfmt,
+--     }
+-- })
